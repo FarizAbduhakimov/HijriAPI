056b66f2442fa63f95a5234283ca407595afcccb
Fariz <farizabduhakimov@gmail.com>
2022-07-30 22:37:22 +0500

    app created

---
+++ hijri_api/hijri/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
+urlpatterns = [
+    path("gregorian/", views.GregorianYearView.as_view()),
+]
