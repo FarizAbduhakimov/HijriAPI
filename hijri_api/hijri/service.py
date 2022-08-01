232361c03c7515ba52df2103afd5cde888fd3267
Fariz <farizabduhakimov@gmail.com>
2022-08-02 00:11:53 +0500

    modified:   hijri_api/hijri/service.py

--- hijri_api/hijri/service.py
+++ hijri_api/hijri/service.py
@@ -5,7 +5,7 @@ from hijri_converter import Hijri, Gregorian
 def get_day(*args, **kwargs):
     greg_day = datetime.date.today().strftime("%Y%m%d")
     hijri_day = Gregorian(int(greg_day)).to_hijri()
-    # hijri_day.strftime("%Y%m%d")
+    hijri_day.strftime("%Y%m%d")
 
     return hijri_day
 
