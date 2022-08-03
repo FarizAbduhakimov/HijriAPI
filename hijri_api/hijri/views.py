90b8ffbfce8abe0f749330ccd8fa06ef9f5bde52
Fariz <farizabduhakimov@gmail.com>
2022-08-03 22:06:38 +0500

    modified:   hijri_api/hijri/__pycache__/serializers.cpython-39.pyc 	modified:   hijri_api/hijri/__pycache__/views.cpython-39.pyc 	modified:   now you can get hijri day

--- hijri_api/hijri/views.py
+++ hijri_api/hijri/views.py
@@ -1,10 +1,9 @@
 import datetime
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from hijri_converter import Gregorian
 from rest_framework.response import Response
-
 
 
 # def date(request):
@@ -17,5 +16,5 @@ from rest_framework.response import Response
 def date(request):
     dt = datetime.date.today()
     hijri_day = Gregorian(dt.year, dt.month, dt.day).to_hijri()
-    hijri_data = {'date': hijri_day}
-    return JsonResponse(hijri_data)
+    # hijri_data = {'date': hijri_day}
+    return HttpResponse(hijri_day)
