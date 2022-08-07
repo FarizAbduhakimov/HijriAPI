1f9dad7780603eaf564a1f7986c529ae3482c101
Fariz <farizabduhakimov@gmail.com>
2022-08-07 22:08:05 +0500

    urls 🔗

--- hijri_api/hijri/urls.py
+++ hijri_api/hijri/urls.py
@@ -4,5 +4,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path("tohijri/", views.to_hijri)
+    path("tohijri/", views.to_hijri),
+    path("togregorian/", views.to_gregorian)
 ]
