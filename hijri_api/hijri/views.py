2c152e19ede9ee90d6be6b5ffc234f7cfd48c60a
Fariz <farizabduhakimov@gmail.com>
2022-08-07 22:10:31 +0500

    added to_greg func

--- hijri_api/hijri/views.py
+++ hijri_api/hijri/views.py
@@ -1,36 +1,40 @@
 import datetime
-from django.http import HttpResponse, JsonResponse
-from dateutil.parser import parse
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from hijri_converter import Gregorian
-from rest_framework.response import Response
+from hijri_converter import Hijri, Gregorian
 
 
 
 
-# def date(request):
-#     dt = datetime.date.today()
-#     hijri_day = Gregorian(dt.year, dt.month, dt.day).to_hijri()
-#     # hijri_data = {'date': hijri_day}
-#     return HttpResponse(hijri_day)
-
-@api_view(['GET','POST'])
+@api_view(['GET'])
 def to_hijri(request):
     year = request.data["year"]
     month = request.data["month"]
     day = request.data["day"]
     dt = datetime.datetime(year, month, day)
-    
-    # dt_value = "{year}-{month}-{day}".format(dt.year,
-    #                                         dt.month,
-    #                                         dt.day)
     hijri = Gregorian(dt.year, dt.month, dt.day).to_hijri()
-    dict = {
+    hijri_date = {
             "year": hijri.year,
             "month": hijri.month,
             "day": hijri.day
         }
     
-    return JsonResponse(dict)
+    return JsonResponse(hijri_date)
 
+
+@api_view(['GET'])
+def to_gregorian(request):
+    """Get hijri date"""
+    year = request.data["year"]
+    month = request.data["month"]
+    day = request.data["day"]
+    dt = datetime.datetime(year, month, day)
+    gregorian = Hijri(dt.year, dt.month, dt.day).to_gregorian()
+
+    gregorian_date = {
+            "year": gregorian.year,
+            "month": gregorian.month,
+            "day": gregorian.day
+        }
+    
+    return JsonResponse(gregorian_date)
