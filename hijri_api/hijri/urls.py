69411ba2b04bc488fcee9860faadd8a4c824aef0
Fariz <farizabduhakimov@gmail.com>
2022-08-08 23:46:42 +0500

    urls updated 🔗

--- hijri_api/hijri/urls.py
+++ hijri_api/hijri/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path("tohijri/", views.to_hijri),
-    path("togregorian/", views.to_gregorian)
+    path("togregorian/", views.to_gregorian),
+    path("months/hijri", views.hijri_months),
+    path("months/gregorian", views.gregorian_months),
+    path("weekdays/hijri", views.hijri_days),
+    path("weekdays/gregorian", views.gregorian_days)
 ]
