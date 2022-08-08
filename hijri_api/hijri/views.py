0577da1a1ae1d139636d405998a7b6dc74be2fc8
Fariz <farizabduhakimov@gmail.com>
2022-08-08 23:54:13 +0500

    views updated, hijri & greg months  added ✅

--- hijri_api/hijri/views.py
+++ hijri_api/hijri/views.py
@@ -2,12 +2,14 @@ import datetime
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from hijri_converter import Hijri, Gregorian
-
+from .months import hijri_months_dict, gregorian_months_dict
+from .weekdays import hijri_days_dict, gregorian_days_dict
 
 
 
 @api_view(['GET'])
 def to_hijri(request):
+    """Get Hijri date"""
     year = request.data["year"]
     month = request.data["month"]
     day = request.data["day"]
@@ -24,7 +26,7 @@ def to_hijri(request):
 
 @api_view(['GET'])
 def to_gregorian(request):
-    """Get hijri date"""
+    """Get gregorian date"""
     year = request.data["year"]
     month = request.data["month"]
     day = request.data["day"]
@@ -41,11 +43,25 @@ def to_gregorian(request):
 
 
 
-# @api_view(['GET'])
-# def hijri_months(request):
-#     pass
+@api_view(['GET'])
+def hijri_months(request):
+    """Hijri months"""
+    return JsonResponse(hijri_months_dict())
 
 
-# @api_view(['GET'])
-# def gregorian_months(request):
-#     pass
+@api_view(['GET'])
+def gregorian_months(request):
+    """Gregorian months"""
+    return JsonResponse(gregorian_months_dict())
+
+
+@api_view(['GET'])
+def hijri_days(request):
+    """Hijri weekdays"""
+    return JsonResponse(hijri_days_dict())
+
+
+@api_view(['GET'])
+def gregorian_days(request):
+    """Gregorian weekdays"""
+    return JsonResponse(gregorian_days_dict())
