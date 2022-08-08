ee67ab97edb8d25ed1e496de268c0c6c50b9e785
Fariz <farizabduhakimov@gmail.com>
2022-08-08 23:42:18 +0500

    Added months dict 🗓

---
+++ hijri_api/hijri/months.py
@@ -0,0 +1,96 @@
+def hijri_months_dict():
+    months = {
+        1: {
+            "en": "Muharram",
+            "ar": "ٱلْمُحَرَّم"
+        },
+        2: {
+            "en": "Safar",
+            "ar": "صَفَر"
+        },
+        3: {
+            "en": "Rabi’ al-Awwal",
+            "ar": "رَبِيع ٱلْأَوَّل"
+        },
+        4: {
+            "en": "Rabi’ al-Thani",
+            "ar": "رَبِيع ٱلثَّانِي"
+        },
+        5: {
+            "en": "Jumada al-Ula",
+            "ar": "جُمَادَىٰ ٱلْأُولَىٰ"
+        },
+        6: {
+            "en": "Jumada al-Akhirah",
+            "ar": "جُمَادَىٰ ٱلْآخِرَة"
+        },
+        7: {
+            "en": "Rajab",
+            "ar": "رَجَب"
+        },
+        8: {
+            "en": "Sha’ban",
+            "ar": "شَعْبَان"
+        },
+        9: {
+            "en": "Ramadhan",
+            "ar": "رَمَضَان"
+        },
+        10: {
+            "en": "Shawwal",
+            "ar": "شَوَّال"
+        },
+        11: {
+            "en": "Dhu al-Qi’dah",
+            "ar": "ذُو ٱلْقَعْدَة"
+        },
+        12: {
+            "en": "Dhu al-Hijjah",
+            "ar": "ذُو ٱلْحِجَّة"
+        }
+    }
+
+    return months
+
+
+def gregorian_months_dict():
+    months = {
+        1: {
+            "en": "January"
+        },
+        2: {
+            "en": "February"
+        },
+        3: {
+            "en": "March"
+        },
+        4: {
+            "en": "April"
+        },
+        5: {
+            "en": "May"
+        },
+        6: {
+            "en": "June"
+        },
+        7: {
+            "en": "July"
+        },
+        8: {
+            "en": "August"
+        },
+        9: {
+            "en": "September"
+        },
+        10: {
+            "en": "October"
+        },
+        11: {
+            "en": "November"
+        },
+        12: {
+            "en": "December"
+        }
+    }
+
+    return months
