35c168681b472ca9eb876a0100631ab79c3f3a85
Fariz <farizabduhakimov@gmail.com>
2022-08-08 23:46:50 +0500

    Serializers

--- hijri_api/hijri/serializers.py
+++ hijri_api/hijri/serializers.py
@@ -1,11 +1,1 @@
-import datetime
-
-from rest_framework import serializers
-from hijri_converter import Hijri, Gregorian
-
-
-# class DateSerializer(serializers.Serializer):
-#     dt = datetime.date.today()
-#     hijri_day = Gregorian(dt.year, dt.month, dt.day).to_hijri()
-
 
