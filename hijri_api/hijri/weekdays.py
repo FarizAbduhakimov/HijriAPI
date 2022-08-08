c20f576018ee309c0f7263480761ccab3605f32e
Fariz <farizabduhakimov@gmail.com>
2022-08-08 23:55:31 +0500

    weekdays added 📅

---
+++ hijri_api/hijri/weekdays.py
@@ -0,0 +1,61 @@
+def hijri_days_dict():
+    week_days = {
+        1: {
+            "en": "al-ʾAḥad",
+            "ar": "ٱلْأَحَد"
+        },
+        2: {
+            "en": "al-Ithnayn",
+            "ar": "الاِثْنَيْن"
+        },
+        3: {
+            "en": "ath-Thulāthāʾ",
+            "ar": "ٱلثُّلَاثَاء"
+        },
+        4: {
+            "en": "al-ʾArbiʿāʾ",
+            "ar": "ٱلْأَرْبِعَاء"
+        },
+        5: {
+            "en": "al-Khamīs",
+            "ar": "ٱلْخَمِيس"
+        },
+        6: {
+            "en": "al-Jumʿah",
+            "ar": "ٱلْجُمْعَة"
+        },
+        7: {
+            "en": "as-Sabt",
+            "ar": "ٱلسَّبْت"
+        }
+    }
+    return week_days
+
+
+def gregorian_days_dict():
+    week_days = {
+        1: {
+            "en": "Sunday"
+        },
+        2: {
+            "en": "Monday"
+        },
+        3: {
+            "en": "Tuesday"
+        },
+        4: {
+            "en": "Wednesday"
+        },
+        5: {
+            "en":  "Thursday"
+        },
+        6: {
+            "en": "Friday"
+        },
+        7: {
+            "en": "Saturday"
+        }
+
+    }
+
+    return week_days
