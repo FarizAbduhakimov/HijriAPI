afd7e62c3424cc964f4c4387761b6ed63542e343
Fariz <farizabduhakimov@gmail.com>
2022-08-09 23:53:56 +0500

    Tests added ✅

--- hijri_api/hijri/tests.py
+++ hijri_api/hijri/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import RequestFactory
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class DateTestCase(APITestCase):
+
+    def test_get_date(self):
+        factory = RequestFactory()
+        url = 'http://127.0.0.1:8000/api/tohijri'
+        my_header = {'year': 2022, 'month': 12, 'day': 12}
+        request = factory.get(url, **my_header)
+        print(request.headers, status.HTTP_200_OK)
+
+
+class MonthsTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_months(self):
+        response = self.client.get('http://127.0.0.1:8000/api/months/hijri')
+        self.assertEqual(response.status_code, 200)
+
+
+class WeekDaysTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_days(self):
+        response = self.client.get('http://127.0.0.1:8000/api/weekdays/hijri')
+        self.assertEqual(response.status_code, 200)
+
