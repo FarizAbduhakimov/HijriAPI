bdadaa6a6bfb789f41b0735ee56bb74228c74239
Fariz <farizabduhakimov@gmail.com>
2022-08-10 22:16:33 +0500

    .gitignore added ✅ 	requirements.txt ✅

--- hijri_api/hijri_api/settings.py
+++ hijri_api/hijri_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRETKEY'
+SECRET_KEY = 'z36=5)f&q^dt$1kiore=l&e(x4b_ea=8%n#q(gep&hdj^^)lt9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
