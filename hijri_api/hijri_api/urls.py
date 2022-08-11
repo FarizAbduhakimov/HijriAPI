44d0b668febd3bc87e0064697abfe732da1af485
Fariz <farizabduhakimov@gmail.com>
2022-08-11 21:26:49 +0500

    urls updated 🔗

--- hijri_api/hijri_api/urls.py
+++ hijri_api/hijri_api/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('hijri.urls')),
 ]
