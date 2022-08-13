f5717e4480853553e78a280fd90136e075290398
Fariz <farizabduhakimov@gmail.com>
2022-08-13 23:19:16 +0500

    yasg url added 🔗

--- hijri_api/hijri_api/urls.py
+++ hijri_api/hijri_api/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hijri.urls')),
 ]
+
+urlpatterns += doc_urls
